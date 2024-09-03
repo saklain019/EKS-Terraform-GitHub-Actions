e86b788872f59d3b56ba57a8ca128f49502ba3b3
saklain019 <142903407+saklain019@users.noreply.github.com>
2024-09-03 15:10:35 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "saklain-bucket"
     region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "dynamodb-file"
+    dynamodb_table = "mydynamo"
     encrypt        = true
   }
 }
