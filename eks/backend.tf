daf267c9653bda9bb6ddaf9515deb4131b6d2a18
saklain019 <142903407+saklain019@users.noreply.github.com>
2024-09-06 23:07:47 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "saklaindemo-bucket"
+    bucket         = "saklain-bucket"
     region         = "ap-south-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "saklain-table"
+    dynamodb_table = "demo-table"
     encrypt        = true
   }
 }
