fc22211212e60d7f9fed5681f4a16a24f80caa42
saklain019 <142903407+saklain019@users.noreply.github.com>
2024-09-16 13:36:03 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "mernstack-project-bucket"
-    region         = "ap-south-1"
+    bucket         = "nvirginia-table"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "firstdb-table"
+    dynamodb_table = "nvirginia-bucket-mern"
     encrypt        = true
   }
 }
